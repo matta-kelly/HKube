6ef005101752047b8d9adf17b0cb39acc1fde9e8
matta-kelly <mattakellyy@gmail.com>
2025-12-12 00:16:14 -0800

    Headscale running

--- scripts/generate-inventory.sh
+++ scripts/generate-inventory.sh
@@ -28,6 +28,9 @@ all:
           ansible_host: ${HEADSCALE_IP}
           ansible_user: root
           ansible_ssh_private_key_file: ${SSH_PUBLIC_KEY_FILE%.pub}
+          headscale_version: "0.23.0"
+          headscale_domain: "${HEADSCALE_DOMAIN}"
+          headscale_base_domain: "${HEADSCALE_BASE_DOMAIN}"
           firewall_allow_ports:
             - { port: "80", proto: "tcp" }
             - { port: "443", proto: "tcp" }
@@ -38,6 +41,7 @@ all:
         home-server:
           ansible_host: ${SERVER_IP}
           ansible_user: ${SERVER_USER:-mkultra}
+          ansible_ssh_private_key_file: ${SSH_PUBLIC_KEY_FILE%.pub}
           server_ip: ${SERVER_IP}
           firewall_allow_ports:
             - { port: "6443", proto: "tcp" }
